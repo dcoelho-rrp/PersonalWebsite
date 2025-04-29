f672fa2575104753181f32bdaf94dc87ec15b52f
dcoelho-rrp <coelho.therrp@gmail.com>
2025-04-29 10:58:07 -0300

    fixed bug: terminal lines would be not inline

--- scripts/terminal.js
+++ scripts/terminal.js
@@ -1,22 +1,30 @@
-// To-do: Fix bug -> when pressing enter only one line appears; and it appears next to the first input field not below it
+// To-do: fix bug ->  I can only add one like to the terminal currently and it stops there.
 
 function terminalInput() {
     const input = document.querySelector(".input");
     const main = document.getElementById("main");
-    let childDiv = document.createElement("div");
-    let childInput = document.createElement("input");
-
-    childDiv.classList.add("idk");
-    childDiv.innerHTML = ">:";
-
-    childInput.type = "text";
-    childInput.classList.add("input");
 
     input.addEventListener("keydown", function(event) {
         if (event.key == "Enter") {
-            console.log("working");
-            main.appendChild(childDiv);
-            main.appendChild(childInput);
+            const wrapper = document.createElement("div");
+            wrapper.style.display = "flex";
+
+            let childDiv = document.createElement("div");
+            let childInput = document.createElement("input");
+
+            childDiv.classList.add("idk");
+            childDiv.innerHTML = ">:";
+
+            childInput.type = "text";
+            childInput.classList.add("input");
+
+            wrapper.appendChild(childDiv);
+            wrapper.appendChild(childInput);
+            
+            main.appendChild(wrapper);
+
+            input.blur();
+            childInput.focus();
         }
     })
 }
